5ca7c616c2f90ef3eac61f8c1cc7484b9f08e403
JOSHNEO_YLAYA <153585895+neoylaya@users.noreply.github.com>
2024-08-14 23:34:26 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,34 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+
+        System.out.println("No matter how deep the night, it always turns to day, eventually.");
+        System.out.println("Title: Naruto: Shippuden");
+        System.out.println("Said by: Naruto Uzumaki");
+        System.out.println("Year: 2007-2017");
+        System.out.println();
+
+        System.out.println("I am no hero. I'm just a man who's doing what he believes in.");
+        System.out.println("Title: My Hero Academia: Two Heroes");
+        System.out.println("Said by: All Might");
+        System.out.println("Year: 2018");
+        System.out.println();
+
+        System.out.println("Sometimes, to save a person, you have to be willing to sacrifice everything.");
+        System.out.println("Title: Death Note");
+        System.out.println("Said by: L Lawliet");
+        System.out.println("Year: 2006-2007");
+        System.out.println();
+
+        System.out.println("You should enjoy the little detours. Perhaps they will teach you something.");
+        System.out.println("Title: Hunter x Hunter");
+        System.out.println("Said by: Ging Freecss");
+        System.out.println("Year: 2011-2014");
+        System.out.println();
+
+        System.out.println("The moment you think of giving up, think of the reason why you held on so long.");
+        System.out.println("Title: Fairy Tail");
+        System.out.println("Said by: Natsu Dragneel");
+        System.out.println("Year: 2009-2019");
+        System.out.println();
+    }
+}
