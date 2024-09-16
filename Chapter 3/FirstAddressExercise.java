661b3d633c8f880e3b04095180f96ce58a83e87d
JOSHNEO_YLAYA <153585895+neoylaya@users.noreply.github.com>
2024-09-16 11:42:08 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+    public static void displayAddress() {
+        System.out.println("Apple Company");
+        System.out.println("8900 U.S. Hwy 14");
+        System.out.println("Crystal Lake, IL 60014");
+        System.out.println("Company: Apple Inc.");
+        
+        
+    }
+    public static void main(String[] args){
+        displayAddress();
+        System.out.println("First Java application");
+    }
+}
